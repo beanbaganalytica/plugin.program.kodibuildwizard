54bcbe0e254dbce246c356ea4f015c87672c1da2
Nicholas Deering <beanbaganalytica@gmail.com>
2023-03-21 17:47:15 -0500

    beanbag changes

--- uservar.py
+++ uservar.py
@@ -14,7 +14,7 @@ ART = os.path.join(PATH, 'resources', 'media')
 #        User Edit Variables                            #
 #########################################################
 ADDONTITLE = '[COLOR cyan][B]kodibuild[/B][/COLOR]Wizard'
-BUILDERNAME = 'ndeering5'
+BUILDERNAME = 'beanbaganalytica'
 EXCLUDES = [ADDON_ID, 'repository.kodibuilds']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program./wiki/Installing-Builds
 BUILDFILE = 'https://textbin.net/raw/xraciz2shb'
@@ -77,7 +77,7 @@ THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT = 'No'
 # You can add \n to do line breaks
-CONTACT = 'Contact us on Github at https://github.com/ndeering5/plugin.program.kodibuildwizard'
+CONTACT = 'Contact us on Github at https://github.com/beanbaganalytica/plugin.program.kodibuildwizard'
 # Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON = os.path.join(ART, 'qricon.png')
 CONTACTFANART = 'http://'
@@ -98,9 +98,9 @@ AUTOINSTALL = 'Yes'
 # Addon ID for the repository
 REPOID = 'repository.kodibuilds'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://raw.githubusercontent.com/ndeering5/ndeering5.github.io/master/repo/zips/addons.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/beanbaganalytica/beanbagrepo/master/repo/zips/addons.xml'
 # Url to folder zip is located in
-REPOZIPURL = 'https://github.com/ndeering5/ndeering5.github.io/blob/master/repository.kodibuilds-2.0.zip'
+REPOZIPURL = 'https://github.com/beanbaganalytica/beanbagrepo/blob/master/repository.kodibuilds-2.0.zip'
 #########################################################
 
 #########################################################
